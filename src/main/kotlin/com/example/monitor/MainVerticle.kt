766e0a89db09a7e145b6d574eb4bcb776e6c373d
Vera <fluxion09@gmail.com>
2021-02-09 22:28:03 +0800

    connect mongodb, temporarily hard code connect uri

--- src/main/kotlin/com/example/monitor/MainVerticle.kt
+++ src/main/kotlin/com/example/monitor/MainVerticle.kt
@@ -1,13 +1,30 @@
 package com.example.monitor
 
+import com.mongodb.WriteConcern
+import com.mongodb.reactivestreams.client.MongoClients
+import com.mongodb.reactivestreams.client.MongoCollection
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.handler.BodyHandler
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import io.vertx.kotlin.coroutines.await
+import org.bson.Document
 
 class MainVerticle : CoroutineVerticle() {
 
+  private val uri = "mongodb://localhost:27017"
+
+  private val dbName = "cars"
+
+  private val colName = "carHourCounts"
+
+  private lateinit var col: MongoCollection<Document>
+
   override suspend fun start() {
+    col = MongoClients.create(uri)
+      .getDatabase(dbName)
+      .getCollection(colName)
+      .withWriteConcern(WriteConcern.ACKNOWLEDGED)
+
     vertx
       .createHttpServer()
       .requestHandler(router())
